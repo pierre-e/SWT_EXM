358679233ac7ce237f56c1e4de01e576185a1fbd
Pierre Zachariasen <pierre.zachariasen@gmail.com>
2014-06-18 16:26:59 +0200

    Implemented TwoDTrackTest

--- SWT_Handin.Tests.Unit/TwoDTrackTest.cs
+++ SWT_Handin.Tests.Unit/TwoDTrackTest.cs
@@ -7,6 +7,9 @@
 ///////////////////////////////////////////////////////////
 
 
+using System;
+using System.Collections.Generic;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace SWT_Handin.Tests.Unit
@@ -14,14 +17,32 @@ namespace SWT_Handin.Tests.Unit
     [TestFixture]
     public class TwoDTrackTest
     {
-        [SetUp]
-        protected void SetUp()
+        [Test]
+        public void PropertySpeed_SetAndGet_True()
         {
+            TwoDTrack testTrack = new TwoDTrack();
+            testTrack.Speed = 100;
+            Assert.AreEqual(100, testTrack.Speed);
         }
 
-        [Test]
-        public void TestTwoDTrack()
+        public void PropertyTag_SetAndGet_True()
         {
+            TwoDTrack testTrack = new TwoDTrack();
+            testTrack.Tag = "TestTag!";
+            Assert.AreEqual("TestTag!", testTrack.Tag);
+        }
+
+        public void PropertyPosition_SetAndGet_True()
+        {
+            var position = Substitute.For<TwoDPosition>();
+            var coordinates = new List<double>();
+            coordinates.Add(20.345);
+            coordinates.Add(70.234);
+            position.Coordinates.Returns(coordinates);
+
+            var testTrack = new TwoDTrack();
+            testTrack.Position = position;
+            Assert.AreEqual(coordinates, testTrack.Position);
         }
     } //end TwoDTrackTest
 } //end namespace UnitTests
