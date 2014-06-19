4dc2f251b339593048efece4c0d1a73b5b450c72
pierre-e <pierre.zachariasen@gmail.com>
2014-06-19 13:24:54 +0200

    Added files

---
+++ SWT_Handin/SWT_Handin/IEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT_Handin
+{
+    public interface IEvent
+    {
+        void HookToDetector();
+        void AttachToAtc(ref ATC airTrafficControl);
+        void CheckEventConditions(List<ITrack> tracks);
+    }
+}
